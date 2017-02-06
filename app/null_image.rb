bd54662fad89da42d2eee3e69aa3a3423410628d
fre14 <femile@blueyonder.co.uk>
2017-02-06 19:41:54 +0000

    Error message exclamation mark

--- app/null_image.rb
+++ app/null_image.rb
@@ -3,23 +3,23 @@ require './app/image_interface.rb'
 class NullImage < ImageInterface
 
   def clear
-    puts "Image not initialized"
+    puts "Image not initialized!"
   end
 
   def colour_vertical(column, row_start, row_end, c)
-    puts "Image not initialized"
+    puts "Image not initialized!"
   end
 
   def colour_horizontal(row, column_start, column_end, c)
-    puts "Image not initialized"
+    puts "Image not initialized!"
   end
 
   def colour_pixel(x, y, c)
-    puts "Image not initialized"
+    puts "Image not initialized!"
   end
 
   def to_s
-    "Image not initialized"
+    "Image not initialized!"
   end
 
 end
