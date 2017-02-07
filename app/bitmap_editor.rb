fd13a5f71e4b4e4cc173c35445a8a0a1d5a36d84
fre14 <femile@blueyonder.co.uk>
2017-02-07 20:20:04 +0000

    Refactor and make vertical and horizontal commands more similar

--- app/bitmap_editor.rb
+++ app/bitmap_editor.rb
@@ -18,12 +18,7 @@ class BitmapEditor
       when /^I (\d{1,3}) (\d{1,3})$/
         width = input.scan(/\d{1,3}/)[0].to_i
         height = input.scan(/\d{1,3}/)[1].to_i
-
-        if height.between?(1, @max_height) && width.between?(1, @max_width)
-          @image = Image.new(height, width)
-        else
-          puts "Height and width need to be between 1 and 250"
-        end
+        i_command(width, height)
 
       when 'C'
         @image.clear
@@ -31,48 +26,22 @@ class BitmapEditor
       when /^L (\d{1,3}) (\d{1,3}) [A-Z]$/
         x_coord = input.scan(/\d{1,3}/)[0].to_i
         y_coord = input.scan(/\d{1,3}/)[1].to_i
-
-        if x_coord.between?(1, @image.width) &&
-           y_coord.between?(1, @image.height)
-          colour = input[-1]
-          @image.colour_pixel(x_coord - 1, y_coord - 1, colour)
-        else
-          puts "Height(width) need to be between 1 and #{@image.height}(#{@image.width})"
-        end
+        colour = input[-1]
+        l_command(x_coord, y_coord, colour)
 
       when /^V (\d{1,3}) (\d{1,3}) (\d{1,3}) [A-Z]$/
         x_coord = input.scan(/\d{1,3}/)[0].to_i
         y_start = input.scan(/\d{1,3}/)[1].to_i
         y_end = input.scan(/\d{1,3}/)[2].to_i
-
-        if !(x_coord.between?(1, @image.width) &&
-             y_start.between?(1, @image.height) &&
-             y_end.between?(1, @image.height))
-          puts "Height(width) need to be between 1 and #{@image.height}(#{@image.width})"
-        elsif y_start > y_end
-          puts "Make sure the start of interval is less than your end of interval"
-        else
-          colour = input[-1]
-          @image.colour_vertical(x_coord - 1, y_start - 1, y_end - 1, colour)
-        end
-
-        y_end.between?(y_start, @image.height)
+        colour = input[-1]
+        v_command(x_coord, y_start, y_end, colour)
 
       when /^H (\d{1,3}) (\d{1,3}) (\d{1,3}) [A-Z]$/
         x_start = input.scan(/\d{1,3}/)[0].to_i
         x_end = input.scan(/\d{1,3}/)[1].to_i
         y_coord = input.scan(/\d{1,3}/)[2].to_i
-
-        if !(x_start.between?(1, @image.width) &&
-             x_end.between?(1, @image.width) &&
-             y_coord.between?(1, @image.height))
-          puts "Height(width) need to be between 1 and #{@image.height}(#{@image.width})"
-        elsif x_start > x_end
-          puts "Make sure the start of interval is less than your end of interval"
-        else
-          colour = input[-1]
-          @image.colour_horizontal(y_coord - 1, x_start - 1, x_end - 1, colour)
-        end
+        colour = input[-1]
+        h_command(x_start, x_end, y_coord, colour)
 
       when 'S'
         puts @image.to_s
@@ -105,5 +74,49 @@ H X1 X2 Y C - Draw a horizontal segment of colour C in row Y between columns X1 
 S - Show the contents of the current image
 X - Terminate the session"
     end
+
+    def i_command(width, height)
+      if height.between?(1, @max_height) && width.between?(1, @max_width)
+        @image = Image.new(height, width)
+      else
+        puts "Height and width need to be between 1 and 250"
+      end
+    end
+
+    def l_command(x, y, colour)
+      if !(x.between?(1, @image.width) && y.between?(1, @image.height))
+        puts "Height(width) need to be between 1 and #{@image.height}(#{@image.width})"
+      else
+        @image.colour_pixel(x - 1, y-1, colour)
+      end
+    end
+
+    def v_command(x, y_start, y_end, colour)
+      if !(line_in_bound(x, @image.width, y_start, y_end, @image.height))
+        puts "Height(width) need to be between 1 and #{@image.height}(#{@image.width})"
+      elsif y_start > y_end
+        puts "Make sure the start of interval is less than your end of interval"
+      else
+        @image.colour_vertical(x - 1, y_start - 1, y_end - 1, colour)
+      end
+
+    end
+
+    def h_command(x_start, x_end, y, colour)
+      if !(line_in_bound(y, @image.height, x_start, x_end, @image.width))
+        puts "Height(width) need to be between 1 and #{@image.height}(#{@image.width})"
+      elsif x_start > x_end
+        puts "Make sure the start of interval is less than your end of interval"
+      else
+        @image.colour_horizontal(y - 1, x_start - 1, x_end - 1, colour)
+      end
+    end
+
+    def line_in_bound(origin, max_origin, start_interval, end_interval, max_interval)
+      origin.between?(1, max_origin) &&
+      start_interval.between?(1, max_interval) &&
+      end_interval.between?(1, max_interval)
+    end
+
 
 end
